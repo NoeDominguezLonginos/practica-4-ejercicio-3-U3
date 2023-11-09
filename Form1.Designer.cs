dd40515fd1d1ab65e2d1ed19fc34a418730c9bd1
LENOVO T450 <LENOVO T450@DESKTOP-S6J3G1K>
2023-11-09 00:03:42 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace practica_4__ejercicio_3_U3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button1 = new Button();
+            listBox1 = new ListBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(200, 56);
+            label1.Name = "label1";
+            label1.Size = new Size(216, 15);
+            label1.TabIndex = 0;
+            label1.Text = "genera los numeos del 1 al 20 con while";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(200, 181);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 45);
+            button1.TabIndex = 1;
+            button1.Text = "imprime en\r\npantalla";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(398, 97);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(164, 229);
+            listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(626, 368);
+            Controls.Add(listBox1);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private ListBox listBox1;
+    }
+}
